0b15f291cff72bfa6b4bf5b877447b81743fe181
xucee90 <cexu@Ces-MacBook-Pro.local>
2015-09-07 18:34:16 -0500

    haha
    ha

---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "httpd" do
+  action :install
+end
+
+service "httpd" do
+  action [:enable, :start]
+end
+
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+  mode "0644"
+end
